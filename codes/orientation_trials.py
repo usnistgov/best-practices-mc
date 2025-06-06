9c823b606ec28ce79f884cdf7eb08a94da216f6d
Harold Hatch <harold.hatch@nist.gov>
2025-06-06 13:55:11 -0400

    defined third angle psi

--- codes/orientation_trials.py
+++ codes/orientation_trials.py
@@ -6,30 +6,43 @@ random.seed(43279857)
 num_sims = 20
 num_trials = int(5e5)
 num_bins = int(3e2)
-def atan2(xs): return np.atan2(xs[:, 1], xs[:, 0])
-def acos(xs): return np.acos(xs[:, 2])
+def atan2(xs): return np.atan2(xs[:, 0, 1], xs[:, 0, 0])
+def acos(xs): return np.acos(xs[:, 0, 2])
+def psi(xs):
+    """
+    Return the azimuthal angle of second vector in a reference frame
+    given by the minimal rotation of the z-axis into the first vector.
+    """
+    angs = np.zeros(len(xs))
+    z = [0, 0, 1]
+    for i,vecs in enumerate(xs):
+        axis = np.cross(z, vecs[0])
+        axis /= np.linalg.norm(axis)
+        rtmx = rotation.axis_angle(axis, np.acos(np.dot(z, vecs[0])))
+        angs[i] = np.atan2(np.dot(vecs[1], np.matmul(rtmx, [1, 0, 0])),
+                           np.dot(vecs[1], np.matmul(rtmx, [0, 1, 0])))
+    return angs
 def azimuth(an): return 0*an + 0.5/np.pi
 def polar(an): return 0.5*np.sin(an)
-angs = {"azimuth0": {"min":-np.pi, "fn":atan2, "dist":azimuth, "i":0},
-        "azimuth1": {"min":-np.pi, "fn":atan2, "dist":azimuth, "i":1},
-        "polar0": {"min":0, "fn":acos, "dist":polar, "i":0},
-        "polar1": {"min":0, "fn":acos, "dist":polar, "i":1}}
+angs = {"azimuth": {"min":-np.pi, "fn":atan2, "dist":azimuth},
+        "polar": {"min":0, "fn":acos, "dist":polar},
+        "psi": {"min":-np.pi, "fn":psi, "dist":azimuth}}
 hist = np.zeros(shape=(num_sims, len(angs), 2, num_bins))
 for sim in range(num_sims):
     x = [[1, 0, 0], [0, 1, 0]]
     xyz = np.zeros(shape=(num_trials, 2, 3))
-    dt_max = np.pi
+    dt_max = np.pi  # randomize first orientation
     for ixyz,_ in enumerate(xyz):
         angle = dt_max*random.uniform(-0.5, 0.5)
-        dt_max = np.pi/10
+        dt_max = np.pi/10  # smaller perturbations after first
         axis = unit_sphere.uniform_surface()
         for ix,_ in enumerate(x):
             x[ix] = np.matmul(rotation.axis_angle(axis, angle), x[ix])
             xyz[ixyz][ix] = x[ix]
     for ia,a in enumerate(angs):
-        cn, bn = np.histogram(angs[a]["fn"](xyz[:, angs[a]["i"]]), bins=num_bins, density=True, range=[angs[a]["min"],np.pi])
-        hist[sim][ia][0] = bn[1:] - 0.5*(bn[1] - bn[0])
-        hist[sim][ia][1] = cn
+        counts, bins = np.histogram(angs[a]["fn"](xyz), bins=num_bins, density=True, range=[angs[a]["min"],np.pi])
+        hist[sim][ia][0] = bins[1:] - 0.5*(bins[1] - bins[0])
+        hist[sim][ia][1] = counts
 for ia,a in enumerate(angs):
     inside = 0
     for ib,b in enumerate(hist[0, ia, 0, :]):
--- figures/orientation_trials.py
+++ figures/orientation_trials.py
@@ -14,23 +14,31 @@ random.seed(43279857)
 num_sims = 20
 num_trials = int(5e5)
 num_bins = int(3e2)
-#num_bins = int(num_trials/1e3)
-def atan2(xs): return np.atan2(xs[:, 1], xs[:, 0])
-def acos(xs): return np.acos(xs[:, 2])
+def atan2(xs): return np.atan2(xs[:, 0, 1], xs[:, 0, 0])
+def acos(xs): return np.acos(xs[:, 0, 2])
+def psi(xs):
+    angs = np.zeros(len(xs))
+    z = [0, 0, 1]
+    for i,vecs in enumerate(xs):
+        axis = np.cross(z, vecs[0])
+        axis /= np.linalg.norm(axis)
+        rtmx = rotation.axis_angle(axis, np.acos(np.dot(z, vecs[0])))
+        angs[i] = np.atan2(np.dot(vecs[1], np.matmul(rtmx, [1, 0, 0])),
+                           np.dot(vecs[1], np.matmul(rtmx, [0, 1, 0])))
+    return angs
 def azimuth(an): return 0*an + 0.5/np.pi
 def polar(an): return 0.5*np.sin(an)
-angs = {"azimuth1": {"min":-np.pi, "fn":atan2, "dist":azimuth, "color":"blue", "i":0},
-        "azimuth2": {"min":-np.pi, "fn":atan2, "dist":azimuth, "color":"green", "i":1},
-        "polar1": {"min":0, "fn":acos, "dist":polar, "color":"red", "i":0},
-        "polar2": {"min":0, "fn":acos, "dist":polar, "color":"orange", "i":1}}
+angs = {"azimuth": {"min":-np.pi, "fn":atan2, "dist":azimuth, "color":"blue"},
+        "polar": {"min":0, "fn":acos, "dist":polar, "color":"red"},
+        "psi": {"min":-np.pi, "fn":psi, "dist":azimuth, "color":"orange"}}
 hist = np.zeros(shape=(num_sims, len(angs), 2, num_bins))
 for sim in range(num_sims):
     x = [[1, 0, 0], [0, 1, 0]]
     xyz = np.zeros(shape=(num_trials, 2, 3))
-    dt_max = np.pi
+    dt_max = np.pi  # randomize first orientation
     for ixyz,_ in enumerate(xyz):
         angle = dt_max*random.uniform(-0.5, 0.5)
-        dt_max = np.pi/10
+        dt_max = np.pi/10  # smaller perturbations of rest
         axis = unit_sphere.uniform_surface()
         for ix,_ in enumerate(x):
             x[ix] = np.matmul(rotation.axis_angle(axis, angle), x[ix])
@@ -38,7 +46,7 @@ for sim in range(num_sims):
     #for ix in [0]:
     #for ix,_ in enumerate(x):
     for ia,a in enumerate(angs):
-        cn, bn = np.histogram(angs[a]["fn"](xyz[:, angs[a]["i"]]), bins=num_bins, density=True, range=[angs[a]["min"],np.pi])
+        cn, bn = np.histogram(angs[a]["fn"](xyz), bins=num_bins, density=True, range=[angs[a]["min"],np.pi])
         hist[sim][ia][0] = bn[1:] - 0.5*(bn[1] - bn[0])
         hist[sim][ia][1] = cn
 for ia,a in enumerate(angs):
@@ -56,6 +64,6 @@ for ia,a in enumerate(angs):
 #plt.show()
 plt.xlabel("Angle", fontsize=16)
 plt.ylabel("Probability Density", fontsize=16)
-plt.annotate("Polar Angles", xy=[1, 0.45], xytext=[-1.1, 0.5], arrowprops=dict(arrowstyle="->", lw=1), fontsize=16)
+plt.annotate("Polar Angle", xy=[1, 0.45], xytext=[-1.1, 0.5], arrowprops=dict(arrowstyle="->", lw=1), fontsize=16)
 plt.annotate("Azimuthal Angles", xy=[-1.5, 0.2], xytext=[-2.5, 0.3], arrowprops=dict(arrowstyle="->", lw=1), fontsize=16)
 plt.savefig(basename+".pdf", transparent=True, dpi=300)

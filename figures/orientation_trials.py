Added quaternions back, fixed a few typos with delta theta max.

figures/orientation_trials.py
@@ -35,7 +35,7 @@ hist = np.zeros(shape=(num_sims, len(angs), 2, num_bins))
 for sim in range(num_sims):
     x = [[1, 0, 0], [0, 1, 0]]
     xyz = np.zeros(shape=(num_trials, 2, 3))
-    dt_max = np.pi  # randomize first orientation
+    dt_max = 2*np.pi  # randomize first orientation
     for ixyz,_ in enumerate(xyz):
         angle = dt_max*random.uniform(-0.5, 0.5)
         dt_max = np.pi/10  # smaller perturbations of rest

figures/random_orientation.py
@@ -17,7 +17,7 @@ random.seed(43279857)
 ax = plt.figure().add_subplot(projection='3d')
 unit_sphere.plot(ax)
 for i in range(int(1e4)):
-    angle = random.uniform(0, np.pi/2.)
+    angle = random.uniform(-np.pi/2, np.pi/2)
     axis = unit_sphere.uniform_surface()
     x = np.matmul(rotation.axis_angle(axis, angle), [0, 0, 1])
     ax.plot([0, x[0]], [0, x[1]], [0, x[2]], color='orange', alpha=0.025)
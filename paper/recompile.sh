d73c3e9b9db6350b923de71abdc1e1c27a9df615
Harold Hatch <harold.hatch@nist.gov>
2025-08-19 14:57:00 -0400

    Organized LaTeX sections by file

--- paper/recompile.sh
+++ paper/recompile.sh
@@ -1,6 +1,6 @@
-#latexdiff paperv0.1.tex paperv1.0.tex > diffv1.0.tex
-for b in paperv1.0; do
-#for b in diffv1.0; do
+#latexdiff paperv1.0.tex paperv1.1.tex > diffv1.1.tex
+for b in paperv1.1; do
+#for b in diffv1.1; do
 #for b in paperv1.0 diffv1.0; do
 
 rm *.aux

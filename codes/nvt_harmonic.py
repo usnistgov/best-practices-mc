a9b4b733167286411c14fcd5bb2ec6346b68c13a
Samiha Sharlin <70553929+ssharlin@users.noreply.github.com>
2026-08-06 09:56:31 -0400

    Tighten spacing in Code Block 1

--- codes/nvt_harmonic.py
+++ codes/nvt_harmonic.py
@@ -16,13 +16,10 @@ def mc_accept(old_energy, new_energy, beta):
     return random.random() < acceptance
 
 def mc_run(beta, n_steps, max_disp=0.5):
-
     position = init_position_harmonic()
     energy = harmonic_energy(position, beta)
-
     positions = np.zeros(n_steps)
     energies = np.zeros(n_steps)
-
     for step in range(n_steps):
         new_position = mc_attempt_trans(position, max_disp)
         new_energy = harmonic_energy(new_position, beta)
@@ -30,15 +27,12 @@ def mc_run(beta, n_steps, max_disp=0.5):
             position, energy = new_position, new_energy
         positions[step] = position
         energies[step] = energy
-
     return positions, energies
 
 def block_avg(values, num_blocks=100, equil=1000):
-    
     production = values[equil:]
     blocks = np.array_split(production, num_blocks)
     block_means = [np.average(block) for block in blocks]
-
     mean = np.average(production)
     standard_error = np.std(block_means) / np.sqrt(num_blocks)
     return mean, standard_error
@@ -47,7 +41,6 @@ if __name__ == "__main__":
     random.seed(43279857)
     beta = random.uniform(1, 2)
     print("Expected energy:", 0.5/beta)
- 
     positions, energies = mc_run(beta, n_steps=int(1e6))
     mean_pos, se_pos = block_avg(positions)
     mean_en, se_en = block_avg(energies)
